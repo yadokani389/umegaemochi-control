3cd42af9437298d09790cabca0a3603ded846c63
yadokani389 <yadokani389@gmail.com>
2024-11-30 09:53:13 +0900

    chore: refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -13,14 +13,19 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
+          overlays = [ rust-overlay.overlays.default ];
           config = {
             allowUnfree = true;
             android_sdk.accept_license = true;
           };
         };
+
         rust-toolchain = pkgs.rust-bin.stable.latest.default.override {
-          targets = [ "x86_64-unknown-linux-gnu" "x86_64-linux-android" "aarch64-linux-android"];
+          targets = [
+            "x86_64-unknown-linux-gnu"
+            "x86_64-linux-android"
+            "aarch64-linux-android"
+          ];
         };
 
         androidComposition = pkgs.androidenv.composeAndroidPackages {
@@ -28,8 +33,7 @@
           toolsVersion = "26.1.1";
           platformToolsVersion = "35.0.1";
           buildToolsVersions = [ "30.0.3" "34.0.0" ];
-          includeEmulator = true;
-          emulatorVersion = "35.1.4";
+          includeEmulator = false;
           platformVersions = [ "28" "29" "30" "33" "34" ];
           includeSources = false;
           includeSystemImages = false;
@@ -43,50 +47,39 @@
           includeExtras = [ "extras;google;gcm" ];
         };
 
-        libraries = with pkgs; [
-          cairo
-          glib
-          pango
-          librsvg
-          cargo-tauri
-          atk.dev
-          libappindicator
-        ];
-
-        buildInputs = (with pkgs; [
-          pkg-config
-          rust-toolchain
-          webkitgtk_4_1
-          gtk3
-          gdk-pixbuf
-          dbus
-          openssl
-          curl
-          wget
-          dbus
-          openssl_3
-          gtk3
-          gdk-pixbuf
-          libsoup_3
-          harfbuzz
-          cairo
-          webkitgtk
-          librsvg
-          nodePackages.pnpm
-          nodejs_18
-          jdk17
-          gradle
-        ]) ++ [ androidComposition.androidsdk ];
-
       in {
         devShell = pkgs.mkShell rec {
-          inherit buildInputs;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+            nodePackages.pnpm
+            rust-toolchain
+            jdk17
+            gradle
+          ];
+
+          buildInputs = (with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ]) ++ [ androidComposition.androidsdk ];
 
           ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
           NDK_HOME = "${ANDROID_HOME}/ndk-bundle";
           GRADLE_OPTS =
             "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidComposition.androidsdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
           XDG_DATA_DIRS =
             "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}";
         };
