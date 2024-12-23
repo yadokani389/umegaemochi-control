1d92e7c10c1f6eca2f301eea83478bccb4e01648
yadokani389 <yadokani389@gmail.com>
2024-12-23 22:42:42 +0900

    chore: add targets

--- flake.nix
+++ flake.nix
@@ -25,6 +25,8 @@
             "x86_64-unknown-linux-gnu"
             "x86_64-linux-android"
             "aarch64-linux-android"
+            "armv7-linux-androideabi"
+            "i686-linux-android"
           ];
         };
 
